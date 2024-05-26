a4bb08b5295e6dbed25d9748fef8ab27cccce794
Hamza029 <nahinhamza029@gmail.com>
2024-05-26 14:50:28 +0600

    Removed dist/ directory from tracking as per .gitignore

--- dist/migrations/schemas/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const userSchema = (table) => {
-    table.increments('Id').primary().unsigned().unique();
-    table.string('Username').notNullable().unique();
-    table.string('Email').notNullable().unique();
-    table.string('Password').notNullable();
-};
-exports.default = userSchema;
