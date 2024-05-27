3cadde5f1fa81f071402711efbc1cdee48288d56
Hamza029 <nahinhamza029@gmail.com>
2024-05-27 16:15:22 +0600

    updated to mouth user routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
-import express, { Express, Request, Response } from 'express';
-import db from './db/db';
-import { UserType, AuthType } from './interfaces';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import path from 'path';
+
+import userRoute from './routes/userRoute';
 
 const envPath = path.join(__dirname + '/../.env');
 dotenv.config({ path: envPath });
@@ -10,23 +10,15 @@ dotenv.config({ path: envPath });
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-const getUserWithId1 = async () => {
-    try {
-        const val = await db<UserType>('User').where('id', 1).first();
-        if (val) {
-            console.log(val.Username, val.Email);
-        } else {
-            throw new Error("Requested user doesn't exist");
-        }
-    } catch (err) {
-        console.log(err);
-    }
-};
+app.use(express.json());
 
-getUserWithId1();
+app.use('/api/users', userRoute);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('all working');
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+        status: 'fail',
+        message: err.message,
+    });
 });
 
 app.listen(port, () => {
