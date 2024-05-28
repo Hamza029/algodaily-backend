ddae19ff6233515d0832ce957995d0b459fdf7ea
Hamza029 <nahinhamza029@gmail.com>
2024-05-28 12:44:02 +0600

    updated initial migration schema

--- src/migrations/schemas/auth.ts
+++ src/migrations/schemas/auth.ts
@@ -2,11 +2,8 @@ import { Knex } from 'knex';
 
 const authSchema = (table: Knex.CreateTableBuilder) => {
     table.increments('Id').primary().unsigned().unique();
-    table.string('Username').notNullable();
-    table.string('Name').notNullable();
-    table.string('Email').notNullable();
-    table.date('JoinDate').notNullable();
-    table.integer('Role').notNullable();
+    table.string('Username').notNullable().unique();
+    table.string('Password').notNullable();
 };
 
 export default authSchema;
--- src/migrations/schemas/user.ts
+++ src/migrations/schemas/user.ts
@@ -4,7 +4,9 @@ const userSchema = (table: Knex.CreateTableBuilder) => {
     table.increments('Id').primary().unsigned().unique();
     table.string('Username').notNullable().unique();
     table.string('Email').notNullable().unique();
-    table.string('Password').notNullable();
+    table.string('Name').notNullable();
+    table.dateTime('JoinDate').notNullable();
+    table.integer('Role').notNullable();
 };
 
 export default userSchema;
